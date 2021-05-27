0f79793fa2ffd732e4d6ae30bd8dbcfdc205c5b6
Muhammad Raihan Muhith <raihan.muhith@gmail.com>
2021-05-27 19:21:34 +0700

    Templating

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,16 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/', function(){
+        return view('layout.master');
+    });
+    
+    Route::get('/home', 'HomeController@index')->name('home');
 });
+
+
+
+Auth::routes();
+
+
