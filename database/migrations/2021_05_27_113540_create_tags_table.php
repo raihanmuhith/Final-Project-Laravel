69a26a336a0b5eef7090768243baefba55300b98
Muhammad Raihan Muhith <raihan.muhith@gmail.com>
2021-05-27 18:39:48 +0700

    Database Migration and Model

---
+++ database/migrations/2021_05_27_113540_create_tags_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama');
+            $table->unsignedBigInteger('berita_id');
+            $table->foreign('berita_id')->references('id')->on('beritas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+    }
+}
