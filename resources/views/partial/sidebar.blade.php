0b04422adcc692e00fd90bd52cc3981701ed5719
hafshah haudli <hafsahhaudli@gmail.com>
2021-05-30 14:41:21 +0700

    CRUD TAG

--- resources/views/partial/sidebar.blade.php
+++ resources/views/partial/sidebar.blade.php
@@ -34,7 +34,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a href="/" class="nav-link">
+            <a href="/tags" class="nav-link">
               <i class="nav-icon fas fa-table"></i>
               <p>
                 Tag
